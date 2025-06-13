0a60285a95bb6966c8e4d27fe436bf96aa4d2264
Sergio Escalante <escalantesergio821@gmail.com>
2025-06-13 17:38:35 -0400

    Fix

---
+++ build.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+# Exit on error
+set -o errexit
+
+# Instalar las dependencias de Python
+pip install -r requirements.txt
+
+# Recolectar archivos estáticos para producción
+# WhiteNoise los servirá automáticamente.
+python manage.py collectstatic --no-input
+
+# Aplicar las migraciones de la base de datos
+python manage.py migrate 
